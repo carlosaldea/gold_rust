5589077c9a6aeb035a89e6242a8f0eb27df83e25
Carlos Aldea <aldea.car@gmail.com>
2022-02-26 23:02:49 +0000

    Completing the job in the UDP examples   - The former client was actually a server. So, it has been renamed.   - Added a real client to have a proper server/client pair

--- udp_client/main.rs
+++ udp_client/main.rs
@@ -1,18 +1,8 @@
 use std::net::UdpSocket;
 
-fn main() -> std::io::Result<()> {
-    {
-        let socket = UdpSocket::bind("127.0.0.1:34254")?;
+fn main() {
 
-        // Receives a single datagram message on the socket. If `buf` is too small to hold
-        // the message, it will be cut off.
-        let mut buf = [0; 10];
-        let (amt, src) = socket.recv_from(&mut buf)?;
-
-        // Redeclare `buf` as slice of the received data and send reverse data back to origin.
-        let buf = &mut buf[..amt];
-        buf.reverse();
-        socket.send_to(buf, &src)?;
-    } // the socket is closed here
-    Ok(())
+    let socket = UdpSocket::bind("127.0.0.1:45234").expect("couldn't bind to address");
+    let message = "hello dear";
+    socket.send_to(message.as_bytes(), "127.0.0.1:34254").expect("couldn't send data");
 }
